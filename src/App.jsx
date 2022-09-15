8a94fd0020cdb801cb0e849f5925d002e97ad013
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-09-15 14:43:23 -0700

    create file structure

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Nav from './components/nav/Nav'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <div> 
+        <h1>Hello World</h1> 
+        <Header />
+        <Nav />
+        <About  />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonial />
+        <Contact />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {  
+  (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
