63f9e9b2763560442d8b9daeebf5c77a9e7b64ce
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-09-20 11:10:27 -0700

    create JSX and Style for Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './Experience.css'
+import {RiCheckboxCircleFill} from 'react-icons/ri'
 
 const Experience = () => {
+  const frontEnd = ["HTML", "CSS", "JavaScript", "React", "Redux", "Material UI"];
+  const backEnd = ["Ruby", "Ruby on Rails", "SQLite", "PostgreSQL", "Active Record", "Sinatra"];
+
+  const renderExperience = (array) => {
+    return array.map((item => {
+      return (
+        <article className='experience_item'>
+          <RiCheckboxCircleFill className='experience_icon' />
+          <div>
+            <h4>{item}</h4>
+            <small className='text-light'>Experienced</small>
+          </div>
+        </article>
+      )
+    }))
+  }
+
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience_container'>
+        
+        <div className='experience_frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience_content'>
+            {renderExperience(frontEnd)}
+          </div>
+        </div>
+        
+        <div className='experience_backend'>
+          <h3>Backend Development</h3>
+          <div className='experience_content'>
+            {renderExperience(backEnd)}
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
