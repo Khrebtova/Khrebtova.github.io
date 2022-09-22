93a895aa0c42ab373b4ff2818343985b53e3da2b
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-09-22 14:12:25 -0700

    create JSX and style for Testimonial component

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -3,7 +3,43 @@ import './Testimonial.css'
 
 const Testimonial = () => {
   return (
-    <section id='testimonial'>Testimonial</section>
+    <section id='testimonial'>
+      <h5>Reviews from clients</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonial_container">
+        
+        <article className='testimonial'>
+          <div className='client_avatar'>
+            <img src="https://i.pinimg.com/474x/4a/3a/78/4a3a782d6609fa600e98972c111a92fd.jpg" alt="client_avatar"/>
+          </div>
+          <h5 className='client_name'>Mike Wazowski</h5>
+          <small className='client_review'> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi.
+          </small>
+        </article>
+        
+        <article className='testimonial'>
+          <div className='client_avatar'>
+            <img src="https://i.pinimg.com/474x/4a/3a/78/4a3a782d6609fa600e98972c111a92fd.jpg" alt="client_avatar"/>
+          </div>
+          <h5 className='client_name'>Mike Wazowski</h5>
+          <small className='client_review'> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi.
+          </small>
+        </article>
+        
+        <article className='testimonial'>
+          <div className='client_avatar'>
+            <img src="https://i.pinimg.com/474x/4a/3a/78/4a3a782d6609fa600e98972c111a92fd.jpg" alt="client_avatar"/>
+          </div>
+          <h5 className='client_name'>Mike Wazowski</h5>
+          <small className='client_review'> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi.
+          </small>
+        </article>
+              
+      </div>
+    </section>
   )
 }
 
