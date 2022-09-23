1469c2d16342b639853aa2e29c61f7eaa9b94ebe
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-09-23 09:18:31 -0700

    create JSX and style for Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import './Footer.css'
+import React from "react"
+import "./Footer.css"
+import {RiFacebookCircleFill, RiInstagramLine, RiTwitterFill} from "react-icons/ri";
+import {BsTreeFill} from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#home" className="footer_logo">KM</a>
+      <ul className="permalinks">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://faceboook.com"><RiFacebookCircleFill /></a>
+        <a href="https://instagram.com"><RiInstagramLine /></a>
+        <a href="https://twitter.com"><RiTwitterFill /></a>
+      </div>
+
+      <div className="footer_copyrights">
+        <small>&copy; All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
