2cd0e81a7551d652224d48a43730b2db8ab696fe
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-09-23 09:20:49 -0700

    added data array for rendering reviews

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,43 +1,62 @@
 import React from 'react'
 import './Testimonial.css'
+//video @2:35 to import swiper 
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 const Testimonial = () => {
+  const reviews = [
+    {
+      id: 1,
+      avatar: 'https://i.pinimg.com/474x/4a/3a/78/4a3a782d6609fa600e98972c111a92fd.jpg',
+      name: 'Mike Wazowski',
+      review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+    },
+    {
+      id: 2,
+      name: 'Sullivan',
+      avatar: 'https://i.pinimg.com/564x/90/10/9e/90109e11b1c4cebb2f030c1dbcb09bd4.jpg',
+      review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+    },
+    {
+      id: 3,
+      avatar: 'https://i.pinimg.com/564x/fa/d8/3b/fad83b229d4c0666e3f7d45035af5404.jpg',
+      name: 'Randal',
+      review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi."
+    }
+  ]
+
+  const renderReviews = reviews.map(review => {
+    return(
+      //<SwiperSlide className='testimonial' key={review.id}>
+      <article className='testimonial' key={review.id}>
+        <div className='client_avatar'>
+          <img src={review.avatar} alt={review.name} />
+        </div>
+        <h5 className='client_name'>{review.name}</h5>
+        <small className='client_review'> 
+          {review.review}
+        </small>
+      </article>
+      // </SwiperSlide>
+    )
+  })
+
   return (
-    <section id='testimonial'>
+    <section id='testimonials'>
       <h5>Reviews from clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonial_container">
-        
-        <article className='testimonial'>
-          <div className='client_avatar'>
-            <img src="https://i.pinimg.com/474x/4a/3a/78/4a3a782d6609fa600e98972c111a92fd.jpg" alt="client_avatar"/>
-          </div>
-          <h5 className='client_name'>Mike Wazowski</h5>
-          <small className='client_review'> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi.
-          </small>
-        </article>
-        
-        <article className='testimonial'>
-          <div className='client_avatar'>
-            <img src="https://i.pinimg.com/474x/4a/3a/78/4a3a782d6609fa600e98972c111a92fd.jpg" alt="client_avatar"/>
-          </div>
-          <h5 className='client_name'>Mike Wazowski</h5>
-          <small className='client_review'> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi.
-          </small>
-        </article>
-        
-        <article className='testimonial'>
-          <div className='client_avatar'>
-            <img src="https://i.pinimg.com/474x/4a/3a/78/4a3a782d6609fa600e98972c111a92fd.jpg" alt="client_avatar"/>
-          </div>
-          <h5 className='client_name'>Mike Wazowski</h5>
-          <small className='client_review'> 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur nemo nisi deserunt accusantium harum fugit sapiente vel, amet ipsum veniam, animi quisquam tempora officiis itaque esse, necessitatibus sequi. Commodi.
-          </small>
-        </article>
-              
+      <div className="container testimonial_container"> 
+        {/* <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={40}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}          
+        > */}
+        {renderReviews}
+        {/* </Swiper>        */}        
       </div>
     </section>
   )
